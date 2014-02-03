e1fcbe3787e70e436441d4812db3ad3f5504c90c
Yserz <michael.koppen@googlemail.com>
2014-02-03 15:54:38 +0100

    - added junit to pom

---
+++ src/test/java/de/fhb/rjcheckexample/manager/AccountManagerTest.java
@@ -0,0 +1,37 @@
+package de.fhb.rjcheckexample.manager;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+public class AccountManagerTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+
+	}
+
+	@Before
+	public void setUp() throws Exception {
+
+	}
+
+	@After
+	public void tearDown() throws Exception {
+
+	}
+
+	@AfterClass
+	public void tearDownAfterClass() throws Exception {
+
+	}
+
+	@Test
+	public void testSample() throws Exception {
+
+	}
+
+}
---
+++ src/test/java/de/fhb/rjcheckexample/repository/AccountRepositoryTest.java
@@ -0,0 +1,37 @@
+package de.fhb.rjcheckexample.repository;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+public class AccountRepositoryTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+
+	}
+
+	@Before
+	public void setUp() throws Exception {
+
+	}
+
+	@After
+	public void tearDown() throws Exception {
+
+	}
+
+	@AfterClass
+	public void tearDownAfterClass() throws Exception {
+
+	}
+
+	@Test
+	public void testSample() throws Exception {
+
+	}
+
+}
